1645b4b09f6bf905a8facf6be7c369592dfc99ce
tmosite80 <tmosite80@gmail.com>
2026-03-01 13:46:57 +0000

    Actualizar dashboard

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown(
 # Data
 # ================================
 ATTENDANCE_SHEET_ID = "1qABgFnVHSI-yYBvy6Ppbm_DMWBnlhnov9q0QV3pdpFY"
-CREDENTIALS_FILE = "credentials.json"
+CREDENTIALS_FILE = CREDENTIALS_FILE
 
 # --------------------------------------------------
 # CONEXIÓN A GOOGLE
